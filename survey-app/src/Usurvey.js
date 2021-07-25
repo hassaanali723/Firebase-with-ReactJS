8a651a00d402d065421351a4e038a0be5f72d130
Hassaan Ali <58141711+hassaanali723@users.noreply.github.com>
2021-07-25 22:30:45 +0500

    Displaying questions for students

--- survey-app/src/Usurvey.js
+++ survey-app/src/Usurvey.js
@@ -29,11 +29,19 @@ class Usurvey extends Component {
 
     }
 
+    answerSelected(){
+
+    }
+    
+    questionSubmit(){
+
+    }
+
     constructor(props) {
         super(props);
         this.state = { 
             uid: uuid.v1(),
-            studentName: '',
+            studentName: 'Hassaan',
             answers: {
                 answer1: '',
                 answer2: '',
@@ -42,6 +50,8 @@ class Usurvey extends Component {
             isSubmitted: false
          };
          this.nameSubmit = this.nameSubmit.bind(this);
+         this.answerSelected = this.answerSelected.bind(this);
+         this.questionSubmit = this.questionSubmit.bind(this);
     }
     render() { 
         var studentName;
@@ -58,10 +68,41 @@ class Usurvey extends Component {
             </div>;
             questions = ''; 
         }
-        else if(this.state.studentName != '' && this.state.isSubmitted == false){
+        else if(this.state.studentName !== '' && this.state.isSubmitted == false){
             studentName = <h2>Welcome to Usurvey, {this.state.studentName}</h2>
-            questions= <p>Hey</p>
+            questions= <div>
+                <h3>Here are some Questions</h3>
+                <form onSubmit = {this.questionSubmit}> 
+
+                    <div className="questions-card">
+                    <label>What kind of courses you like the most:</label> <br/>
+                    <input type="radio" name="answer1" value="Technology" onChange={this.answerSelected} />Technology
+                    <input type="radio" name="answer1" value="Design" onChange={this.answerSelected} />Design
+                    <input type="radio" name="answer1" value="Marketing" onChange={this.answerSelected} />Marketing
+                    </div>
+
+                    <div className="questions-card">
+                    <label>You are a:</label> <br/>
+                    <input type="radio" name="answer2" value="Student" onChange={this.answerSelected} />Student
+                    <input type="radio" name="answer2" value="Employee" onChange={this.answerSelected} />Employee
+                    <input type="radio" name="answer2" value="Looking-for-job" onChange={this.answerSelected}/>Looking for job
+                    </div>
+
+                    <div className="questions-card">
+                    <label>Is online learning helpful:</label> <br/>
+                    <input type="radio" name="answer2" value="Yes" onChange={this.answerSelected} />Yes
+                    <input type="radio" name="answer2" value="No" onChange={this.answerSelected} />No
+                    <input type="radio" name="answer2" value="Maybe" onChange={this.answerSelected}/>Maybe
+                    </div>
+
+                    <input className="submit-btn" type="button" value="submit" />
+
+
+                </form>
+            </div>
         }
+
+        
 
         return ( 
             <div>
